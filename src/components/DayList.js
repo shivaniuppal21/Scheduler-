5a532332043f209dfa2422cb441ef97cb351cb46
shivani <shivaniuppal21@gmail.com>
2021-02-15 19:36:42 -0500

    first component

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DayListItem from 'components/DayListItem';
+
+export default function DayList(props) {
+    const schedule = props.days.map(dayObj => {
+
+    return(
+        <DayListItem
+            key={dayObj.id}
+            selected={dayObj.name === props.day}
+            setDay={props.setDay}
+            {...dayObj}
+        />
+      )
+    })
+
+  return <ul>{schedule}</ul>
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+const formatSpots = spots => {
+    if (!spots) {
+      return `no spots remaining`;
+    }
+  
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+  
+    return `${spots} spots remaining`;
+  }
+
+export default function DayListItem(props) {
+    const availabilityMessage = formatSpots(props.spots);
+
+    const dayClass = classnames("day-list__item", {
+        'day-list__item--selected': props.selected,
+        'day-list__item--full': !props.spots
+      });
+
+  return (
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{availabilityMessage}</h3>
+    </li>
+  );
+}
